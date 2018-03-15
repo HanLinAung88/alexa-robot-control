4a7d856938cca7ec02bc971f0b1d17178eb96aaa
hanlin <hanlaung@stanford.edu>
2018-03-15 12:48:18 -0700

    Debug code for direction

--- apps/robot/index.js
+++ apps/robot/index.js
@@ -78,6 +78,7 @@ app.intent("RobotMovementIntent", {
     console.log("Direction:");
     console.log(direction);
     var directionCode = directionToCode(direction);
+    console.log(directionCode);
     var dir = directionsCodes[directionCode][0];
     console.log(dir);
     var message = new gcm.Message({
